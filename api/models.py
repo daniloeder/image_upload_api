11a429f91f5c7a893a1c74e2bbfeb652f7846ce3
daniloeder <daniloeder1997@gmail.com>
2023-09-18 12:20:32 -0300

    Solved migrations

--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User as DjangoUser
 
-# Create your models here.
+class Tier(models.Model):
+    name = models.CharField(max_length=100)
+    thumbnail_sizes = models.JSONField()
+    allow_original_link = models.BooleanField()
+    allow_expiring_link = models.BooleanField()
+    
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    user = models.ForeignKey(DjangoUser, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='uploads/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    tier = models.ForeignKey(Tier, on_delete=models.PROTECT)
+    expiring_link_seconds = models.IntegerField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"Image by {self.user.username} uploaded at {self.uploaded_at}"
+
