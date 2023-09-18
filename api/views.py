c69fcacd3eea60591f98797700fba2803d3955b9
daniloeder <daniloeder1997@gmail.com>
2023-09-18 12:26:04 -0300

    Created basic views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Image, Tier
+from .serializers import UserSerializer, ImageSerializer, TierSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class ImageViewSet(viewsets.ModelViewSet):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+
+class TierViewSet(viewsets.ModelViewSet):
+    queryset = Tier.objects.all()
+    serializer_class = TierSerializer
