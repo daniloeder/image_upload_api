42f32ec8cbb05c91d7f626d7fbb069a218b13940
daniloeder <daniloeder1997@gmail.com>
2023-09-18 15:23:28 -0300

    Created admin interface

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Image, Tier
 
-# Register your models here.
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'image', 'uploaded_at', 'tier', 'expiring_link_seconds')
+    list_filter = ('tier',)
+    search_fields = ('user__username', 'image')
+    date_hierarchy = 'uploaded_at'
+
+@admin.register(Tier)
+class TierAdmin(admin.ModelAdmin):
+    list_display = ('name', 'allow_original_link', 'allow_expiring_link')
