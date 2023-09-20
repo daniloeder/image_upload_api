af300eadc339762a410a3062753a235fcd17cb38
daniloeder <daniloeder1997@gmail.com>
2023-09-19 21:01:27 -0300

    Fixed missing options in the Tier

--- api/models.py
+++ api/models.py
@@ -2,36 +2,40 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Tier(models.Model):
-    # Fields for the Tier model
     name = models.CharField(max_length=100)
     thumbnail_sizes = models.CharField(max_length=255, help_text="Comma-separated list of thumbnail sizes (e.g., '200x200,400x400')")
     allow_original_link = models.BooleanField()
     allow_expiring_link = models.BooleanField()
-
-    # String representation of Tier
+    
     def __str__(self):
         return self.name
 
 class Image(models.Model):
-    # Fields for the Image model
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='uploads/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    tier = models.ForeignKey(Tier, on_delete=models.PROTECT)
+    
+    # Define choices for the 'tier' field
+    TIER_CHOICES = [
+        ('Basic', 'Basic'),
+        ('Premium', 'Premium'),
+        ('Enterprise', 'Enterprise'),
+    ]
+    
+    tier = models.CharField(max_length=20, choices=TIER_CHOICES)
     expiring_link_seconds = models.PositiveIntegerField(null=True, blank=True, help_text="Expiring link duration in seconds (between 300 and 30000)")
-
-    # String representation of Image
+    
     def __str__(self):
         return f"Image by {self.user.username} uploaded at {self.uploaded_at}"
 
 class CustomTier(models.Model):
-    # Fields for the CustomTier model
     name = models.CharField(max_length=100)
     allow_original_link = models.BooleanField(default=False)
     allow_expiring_link = models.BooleanField(default=False)
     thumbnail_sizes = models.CharField(max_length=255, help_text="Comma-separated list of thumbnail sizes (e.g., '200x200,400x400')")
-    expiring_link_seconds = models.PositiveIntegerField(help_text="Expiring link duration in seconds (between 300 and 30000)")
+    expiring_link_seconds = models.PositiveIntegerField(
+        help_text="Expiring link duration in seconds (between 300 and 30000)"
+    )
 
-    # String representation of CustomTier
     def __str__(self):
         return self.name
