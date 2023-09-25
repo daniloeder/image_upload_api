2a2de52de58ba7b8e8e78524f31a1af00aa488eb
daniloeder <daniloeder1997@gmail.com>
2023-09-24 23:28:20 -0300

    Implemented Arbitrary thumbnail size

--- api/models.py
+++ api/models.py
@@ -10,11 +10,16 @@ class Tier(models.Model):
     def __str__(self):
         return self.name
 
-
 def get_tiers():
     """Returns a list of all tiers."""
     tiers = Tier.objects.all()
     return tiers
+
+
+def get_tier(tier_name):
+    """Returns a Tier object for the given tier name."""
+    tier = Tier.objects.get(name=tier_name)
+    return tier
 
 
 class Image(models.Model):
@@ -22,6 +27,7 @@ class Image(models.Model):
     image = models.ImageField(upload_to='uploads/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
     expiring_link_seconds = models.PositiveIntegerField(null=True, blank=True, help_text="Expiring link duration in seconds (between 300 and 30000)")
+    arbitrary_thumbnail_size = models.PositiveIntegerField(null=True, blank=True)
 
     TIER_CHOICES = [
         (tier.name, tier.name) for tier in get_tiers()
@@ -31,38 +37,46 @@ class Image(models.Model):
 
     def generate_links(self):
         """Generates and sets the basic_link, premium_link, original_link, and expiring_link fields according to the user tier."""
-        
-        self.link_200px = self.generate_basic_link()
 
-        if self.tier == 'Premium':
-            self.link_400px = self.generate_premium_link()
-        elif self.tier == 'Enterprise':
-            self.link_400px = self.generate_premium_link()
+        links = {}
 
-        if self.tier.allow_original_link:
-            self.original_link = self.generate_original_link()
+        links['link_200px'] = self.generate_basic_link()
 
-        if self.tier.allow_expiring_link:
-            self.expiring_link = self.generate_expiring_link()
+        if self.tier == 'Premium' or self.tier == 'Enterprise':
+            links['link_400px'] = self.generate_premium_link()
 
-        self.save()
+        if get_tier(self.tier).thumbnail_sizes and self.arbitrary_thumbnail_size:
+            links['arbitrary_thumbnail_size'] = self.generate_arbitrary_thumbnail_size()
+
+        if get_tier(self.tier).allow_original_link:
+            links['original_link'] = self.generate_original_link()
+
+        if get_tier(self.tier).allow_expiring_link and self.expiring_link_seconds:
+            links['expiring_link'] = self.generate_expiring_link()
+
+        return links
+
+
+    def generate_arbitrary_thumbnail_size(self):
+        arbitrary_thumbnail_size = "http://localhost:8000/api/images/thumbnails/image_{}px/{}".format(self.arbitrary_thumbnail_size, self.image.name)
+        return arbitrary_thumbnail_size
 
     def generate_basic_link(self):
         """Generates a link to a thumbnail that is 200px in height."""
-        thumbnail_url = "http://localhost:8000/api/images/thumbnails/basic_{}_{}".format(200, self.image.name)
+        thumbnail_url = "http://localhost:8000/api/images/thumbnails/image_200px/{}".format(self.image.name)
         return thumbnail_url
 
     def generate_premium_link(self):
         """Generates a link to a thumbnail that is 200px in height and a link to a thumbnail that is 400px in height."""
-        thumbnail_400px_url = "http://localhost:8000/api/images/thumbnails/basic_{}_{}".format(400, self.image.name)
+        thumbnail_400px_url = "http://localhost:8000/api/images/thumbnails/image_400px/{}".format(400, self.image.name)
         return thumbnail_400px_url
 
     def generate_original_link(self):
         """Generates a link to the originally uploaded image."""
-        original_link = "http://localhost:8000/api/images/uploads/{}".format(self.image.name)
+        original_link = "http://localhost:8000/api/images/{}".format(self.image.name)
         return original_link
 
     def generate_expiring_link(self):
         """Generates an expiring link to the image. The link expires after a given number of seconds, which can be specified by the user."""
-        expiring_link = "http://localhost:8000/api/images/expiring/{}/{}".format(self.image.name, self.expiring_link_seconds)
+        expiring_link = "http://localhost:8000/api/images/expiring_{}_seconds/{}".format(self.expiring_link_seconds, self.image.name)
         return expiring_link
--- api/serializers.py
+++ api/serializers.py
@@ -5,6 +5,7 @@ class ImageSerializer(serializers.ModelSerializer):
     link_200px = serializers.CharField(max_length=255, read_only=True)
     link_400px = serializers.CharField(max_length=255, read_only=True)
     original_link = serializers.CharField(max_length=255, read_only=True)
+    expiring_link = serializers.CharField(max_length=255, read_only=True)
 
     class Meta:
         model = Image
@@ -13,10 +14,15 @@ class ImageSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         """Serializes the Image object."""
         data = super().to_representation(instance)
-        data['link_200px'] = instance.generate_basic_link()
-        data['link_400px'] = instance.generate_premium_link()
+
+        links = instance.generate_links()
+        if links is None:
+            return data
+
+        data.update(links)
 
         return data
+
 
 class TierSerializer(serializers.ModelSerializer):
     class Meta:
